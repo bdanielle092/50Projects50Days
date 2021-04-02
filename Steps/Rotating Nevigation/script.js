3e7505caed38579b209245cdf9ab8f8700ad5eed
Faith Magras <bdanielle092@gmail.com>
2021-04-02 18:46:01 -0500

    changed the text

--- Steps/Rotating Nevigation/script.js
+++ Steps/Rotating Nevigation/script.js
@@ -7,3 +7,7 @@ open.addEventListener('click', () => container.classList.add('show-nav'))
 
 //add an event listener that listens for the click and then a function that closes the nav
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+
+
+
