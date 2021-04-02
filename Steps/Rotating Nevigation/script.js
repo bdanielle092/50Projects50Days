09f8bc8a370350341ad5bf6a008e04ffb1fab00b
Faith Magras <bdanielle092@gmail.com>
2021-04-02 18:14:08 -0500

    finished nav bar"

---
+++ Steps/Rotating Nevigation/script.js
@@ -0,0 +1,9 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+//add an event listener that listens for the click and then a function that show the nav
+open.addEventListener('click', () => container.classList.add('show-nav'))
+
+//add an event listener that listens for the click and then a function that closes the nav
+close.addEventListener('click', () => container.classList.remove('show-nav'))
